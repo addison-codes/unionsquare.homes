1015238105499979379b7c29af07b3ff7682a001
Addison Wanlass <hi@addison.codes>
2023-11-14 12:32:36 -0500

    chore: add analytics tag

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,28 @@
+import Head from 'next/head';
+import Script from 'next/script';
 import React from 'react';
 import 'styles/main.scss';
 
 function App({ Component, pageProps }) {
       return (
+            <>
+                  <Head>
+                        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+                        <meta name="description" content="Union Square Condos" />
+                        <meta name="keywords" content="Union Square Condos, Union Square, Union Square Condos Grand Rapids, Union Square Condos Grand Rapids MI, Union Square Condos, Condos Grand Rapids, Downtown Grand Rapids, Condos Downtown Grand Rapids" />
+                        <title>Union Square Condos</title>
+                  </Head>
+                  <Script src='https://www.googletagmanager.com/gtag/js?id=G-95QBVYP6JD'/>
+                  <Script id='google-analytics' strategy='afterInteractive'>
+                        {`
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', 'G-95QBVYP6JD');
+                        `}
+                  </Script>
             <Component {...pageProps} />
+            </>
       );
     }
 
